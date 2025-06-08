24b691603a966a356a0597b797ad75c6ebca4386
kennylin894 <Kennylin122004@gmail.com>
2025-06-08 13:49:51 -0400

    did all the queries

--- queries.sql
+++ queries.sql
@@ -14,3 +14,30 @@ USE car_dealership;
 SELECT * 
 FROM vehicles 
 WHERE VIN = 'VIN';
+
+-- 4. Find the dealership where a certain car is located, by VIN
+USE car_dealership;
+SELECT d.dealership_id, d.name, d.address, d.phone
+FROM dealerships d
+JOIN inventory i ON d.dealership_id = i.dealership_id
+WHERE i.VIN = 'VIN';
+
+-- 5. Find all Dealerships that have a certain car type (i.e. Red Ford Mustang)
+USE car_dealership;
+SELECT DISTINCT d.dealership_id, d.name, d.address, d.phone
+FROM dealerships d
+JOIN inventory i ON d.dealership_id = i.dealership_id
+JOIN vehicles v ON i.VIN = v.VIN
+WHERE v.make = 'MAKE' 
+  AND v.model = 'MODEL' 
+  AND v.color = 'COLOR';
+  
+-- 6. Get all sales information for a specific dealer for a specific date range
+USE car_dealership;
+SELECT sc.*, v.make, v.model, v.year, v.color
+FROM sales_contracts sc
+JOIN vehicles v ON sc.VIN = v.VIN
+JOIN inventory i ON v.VIN = i.VIN
+WHERE i.dealership_id = 1
+  AND sc.sale_date BETWEEN 'START' AND 'END';  
+  
--- schema.sql
+++ schema.sql
@@ -4,13 +4,16 @@ CREATE DATABASE car_dealership;
 
 USE car_dealership;
 
+-- Table 1: dealerships
 CREATE TABLE dealerships (
-    dealership_id INT NOT NULL,
+    dealership_id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     address VARCHAR(50) NOT NULL,
-    phone VARCHAR(12) NOT NULL
+    phone VARCHAR(12) NOT NULL,
+    PRIMARY KEY (dealership_id)
 );
 
+-- Table 2: vehicles  
 CREATE TABLE vehicles (
     VIN VARCHAR(17) NOT NULL,
     year INT NOT NULL,
@@ -20,6 +23,8 @@ CREATE TABLE vehicles (
     price DECIMAL(10, 2) NOT NULL,
     mileage INT NOT NULL,
     type VARCHAR(20) NOT NULL,
-    SOLD BOOLEAN DEFAULT FALSE
+    SOLD BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (VIN)
 );
+
 
