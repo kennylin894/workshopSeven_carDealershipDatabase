bc03ffe9941e02e22d0d3b9bd03b31d2279231a4
kennylin894 <Kennylin122004@gmail.com>
2025-06-08 13:07:33 -0400

    made more queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,16 @@
 -- 1. Get all dealerships
 USE car_dealership;
 SELECT * FROM dealerships;
+
+-- 2. Find all vehicles for a specific dealership
+USE car_dealership;
+SELECT v.*
+FROM vehicles v
+JOIN inventory i ON v.VIN = i.VIN
+WHERE i.dealership_id = 1;
+
+-- 3. Find a car by VIN
+USE car_dealership;
+SELECT * 
+FROM vehicles 
+WHERE VIN = 'VIN';
